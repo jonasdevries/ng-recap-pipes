0c5d39bfa0f42ae469df8d3fe03678ba30f02a25
Jonas Devries <jonas@good-it.be>
2020-04-02 09:23:44 +0200

    add to impl pipes reverse and sort

---
+++ src/app/reverse.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'reverse'
+})
+export class ReversePipe implements PipeTransform {
+  private reverseValue: string;
+
+  transform(value: any): any {
+    return value;
+  }
+
+}
